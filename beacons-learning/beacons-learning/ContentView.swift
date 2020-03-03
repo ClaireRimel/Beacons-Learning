853225f36322727b355a0557545c4771e81496f2
ClaireRimel <sivadiercl@gmail.com>
2020-03-03 20:06:41 +0100

    New: project beacons-learning

---
+++ beacons-learning/beacons-learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beacons-learning
+//
+//  Created by Claire on 03/03/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
